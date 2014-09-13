7876600ac60d73d2c5bfdbb0c56f4040f458e8d4
Dan Newport <Dan.Newport@detroitlabs.com>
2014-09-13 14:38:20 -0400

    import scanner, add scanner myScan

--- src/com/example/CalculatorApp/CalculatorApp.java
+++ src/com/example/CalculatorApp/CalculatorApp.java
@@ -3,8 +3,10 @@ package com.example.CalculatorApp;
 /**
  * Created by admin on 9/13/14.
  */
+import java.util.Scanner;
+
 public class CalculatorApp {
     public static void main(String[] args) {
-        
+        Scanner myScan = new Scanner(System.in);
     }
 }
