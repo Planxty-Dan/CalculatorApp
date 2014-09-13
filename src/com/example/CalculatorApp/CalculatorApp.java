d4141f0dcf0474e46c4ef620e8956e2f3ec2f869
Dan Newport <Dan.Newport@detroitlabs.com>
2014-09-13 15:05:40 -0400

    add print statement instructions

--- src/com/example/CalculatorApp/CalculatorApp.java
+++ src/com/example/CalculatorApp/CalculatorApp.java
@@ -8,5 +8,16 @@ import java.util.Scanner;
 public class CalculatorApp {
     public static void main(String[] args) {
         Scanner myScan = new Scanner(System.in);
+
+        System.out.println("Welcome to Calculator\n" +
+                "You can enter numbers to calculate and they will be calculated!\n" +
+                "You know you're impressed.\n" +
+                "Enter a number, then hit enter\n" +
+                "Then enter +, -, *, /, or √\n" +
+                "Then enter the second number for the calculation.\n" +
+                "For the square root you only need to enter the first number and √," +
+                "The square root symbol is entered by holding 'alt' and 'v'");
+
+        
     }
 }
