7e1eae68ab8e1e74a318fff687a58d5920ac58fd
Dan Newport <Dan.Newport@detroitlabs.com>
2014-09-14 23:57:19 -0400

    add for statements to print/calculate each math operatot, fix while loop to == instead of =, fix getNum method catch, add print/scan to get user input to do another calculation

--- src/com/example/CalculatorApp/CalculatorApp.java
+++ src/com/example/CalculatorApp/CalculatorApp.java
@@ -15,6 +15,7 @@ public class CalculatorApp {
         Scanner myScan = new Scanner(System.in);
         double firstNum = 0;
         double secondNum = 0;
+        String answer;
         String calcAction;
         boolean progLoop = true;
 
@@ -27,16 +28,36 @@ public class CalculatorApp {
                 "For the square root you only need to enter the first number and √," +
                 "The square root symbol is entered by holding 'alt' and 'v'");
 
-        while (progLoop = true) {
+        while (progLoop == true) {
+            System.out.println("\nPlease enter first number:");
             firstNum =getNumbers();
             for (;;) {
+                System.out.println("Please enter +, -, *, /, or √ (alt v):");
                 calcAction = myScan.nextLine();
                 if (calcAction.equals("+") || calcAction.equals("-") || calcAction.equals("*") || calcAction.equals("/") || calcAction.equals("√"))
                     break;
                 else
                     System.out.println("Error: please enter +, -, *, /, or √ (alt v):\n");
             }
-            secondNum =getNumbers();
+            if (calcAction.equals("√"))
+                System.out.println(firstNum + calcAction + " = " + (firstNum * firstNum));
+
+            else {
+                System.out.println("Please enter second number:");
+                secondNum = getNumbers();
+                if (calcAction.equals("+"))
+                    System.out.println(firstNum + " " + calcAction + " " + secondNum + " = " + (firstNum + secondNum));
+                if (calcAction.equals("-"))
+                    System.out.println(firstNum + " " + calcAction + " " + secondNum + " = " + (firstNum - secondNum));
+                if (calcAction.equals("*"))
+                    System.out.println(firstNum + " " + calcAction + " " + secondNum + " = " + (firstNum * secondNum));
+                if (calcAction.equals("/"))
+                    System.out.println(firstNum + " " + calcAction + " " + secondNum + " = " + (firstNum / secondNum));
+            }
+            System.out.println("\nDo another calculation? (y/n)\n");
+            answer = myScan.nextLine();
+            if (answer.toLowerCase().equals("n"))
+                progLoop = false;
         }
     }
 
@@ -44,11 +65,13 @@ public class CalculatorApp {
         Scanner getNumScan = new Scanner(System.in);
         for (;;)
             try {
-                System.out.println("\nPlease enter first number:");
                 double getNum = getNumScan.nextDouble();
                 return getNum;
         }   catch (InputMismatchException e){
-                System.out.println("Not a valid number entry, please try again.\n");
+                System.out.println("Not a valid number entry, please try again.\n" +
+                        "Please enter a valid number:");
+                getNumScan.nextLine();
+
         }
     }
 }
